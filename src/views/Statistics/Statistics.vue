1e9af2e42d8bbb6b77603969a5e7515d94d99164
Ekaterina Gibova <katyagibova@bk.ru>
2022-05-14 20:25:29 +0300

    ASD

--- src/views/Statistics/Statistics.vue
+++ src/views/Statistics/Statistics.vue
@@ -2,45 +2,49 @@
   <div>
     <v-container>
       <v-row>
-        <v-col>
-          <div style="color: rgba(0, 0, 0, 0.6)">
-            СТАТИСТИКА
+        <v-col class="pt-5">
+          <div style="color: rgba(0, 0, 0, 1)">
+            Статистика
           </div>
         </v-col>
       </v-row>
       <v-row>
-        <v-col class="pa-0">
-          <v-divider></v-divider>
+        <v-col>
+          <v-carousel v-model="carousel" show-arrows-on-hover >
+            <v-carousel-item
+            v-for="(elem, i) in carouselLength"
+            :key="i">
+              <!-- <v-sheet
+              height="100%"
+              tile> -->
+                <v-row
+                align="center"
+                class="fill-height">
+                  <v-col 
+                  cols="4"
+                  class="py-0"
+                  align="center"
+                  justify="center"
+                  v-for="(elem, el) in carouselItems"
+                  :key="el"
+                  >
+                    <!-- <div
+                  class="d-flex"> -->
+                      <!-- <v-avatar
+                      tile
+                      size="240"
+                      > -->
+                        <v-img
+                        src="../../assets/camera.png"
+                        ></v-img>
+                      <!-- </v-avatar> -->
+                    <!-- </div>                     -->
+                  </v-col>
+                </v-row>
+              <!-- </v-sheet> -->
+            </v-carousel-item>
+          </v-carousel>
         </v-col>
-      </v-row>
-      <v-row>
-          <v-col>
-              <v-carousel v-model="carousel" >
-                <v-carousel-item
-                v-for="(elem, i) in carouselLength"
-                :key="i"
-                >
-                <v-sheet
-                    height="100%"
-                    tile
-                >
-                    <v-row
-                    class="fill-height"
-                    align="center"
-                    justify="center"
-                    >
-                        <v-col cols="6" 
-                        align="center"
-                        justify="center"
-                        v-for="(elem, el) in carouselItems"
-                        :key="el">
-                            {{elem}} + {{i}}
-                        </v-col>
-                    </v-row>
-                </v-sheet>
-                </v-carousel-item>
-            </v-carousel>
-          </v-col>
       </v-row>
     </v-container>
     
@@ -59,19 +63,19 @@ export default Vue.extend({
   data() {
     return {
       carousel: 0,
-      items: ['item1', 'item2', 'item3', 'item4', 'item5', 'item6'],
+      items: ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10'],
     }
   },
   computed: {
       carouselLength(){
-        return Math.ceil(this.items.length/4)
+        const length = Math.ceil(this.items.length/6)
+        return length
       },
       carouselItems(){
-        let num = this.carousel
-        return this.items.slice(num*4, num*4+4)
-        // 0 - 4
-        //4 - 8
-        //8 - 12
+        const num = this.carousel
+        const items = this.items as string[]
+        const arr = items.slice(num*6, num*6+6)
+        return arr
       },
   }
 });
