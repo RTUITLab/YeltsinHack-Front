20e3ecc74c3562e39df372a8129eca861383e895
Kirill <kirill.baikin@gmail.com>
2022-05-14 10:03:42 +0300

    Some changes

--- src/components/CameraPicker/CameraPicker.vue
+++ src/components/CameraPicker/CameraPicker.vue
@@ -1,9 +1,22 @@
 <template>
   <div :class="$style.parent">
     <div :class="$style.videoContainer">
-      <div :class="$style.svgParent">
+      <div :class="$style.svgParent" @click="onClick">
         <svg>
-          <polyline points="0,0 50,0 150,100 250,100 300,150" fill="transparent" stroke-width="2" stroke="rgb(0,255,0)"/>          <circle cx="102" cy="102" r="5" fill="rgb(0,255,0)" stroke-width="1" stroke="rgb(0,0,0)"/>
+          <polyline
+            :points="points.join(' ')"
+            fill="transparent"
+            stroke-width="2"
+            stroke="rgb(0,255,0)"
+          />
+          <circle
+            cx="102"
+            cy="102"
+            r="5"
+            fill="rgb(0,255,0)"
+            stroke-width="1"
+            stroke="rgb(0,0,0)"
+          />
         </svg>
       </div>
       <img
